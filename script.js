fa57d18fffdea785c17ddfe7318fae6664e11638
Filip Vrubel <114007566+FilipVrubel@users.noreply.github.com>
2024-07-16 10:51:57 +0200

    Implement getting computer choice

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello World!")
+let getRandom = () => Math.floor(Math.random() * 100);
+
+function getComputerChoice() {
+    let num = getRandom();
+    return num % 3 == 0 ? "rock" : num % 3 == 1 ? "paper" : "scissors";
+}
+
